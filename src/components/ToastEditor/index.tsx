dd48d3c9ee1314d75872c8af06e941f35cb285e6
lee jeongmin <danmin20@khu.ac.kr>
2021-04-21 23:43:07 +0900

    canvas 가져오기

--- src/components/ToastEditor/index.tsx
+++ src/components/ToastEditor/index.tsx
@@ -1,8 +1,38 @@
 /// <reference path="react-image-editor.d.ts" />
 import ImageEditor from "@toast-ui/react-image-editor";
+import { useEffect, useState } from "react";
 import "tui-image-editor/dist/tui-image-editor.css";
 
 const ToastEditor = () => {
+  const [lowerCanvas, setLowerCanvas] = useState<HTMLCanvasElement>();
+  // console.log(
+  //   document.getElementsByClassName("lower-canvas")[0]?.toDataURL("image/png")
+  // );
+  console.log("c");
+
+  // const [upperCanvas, setUpperCanvas] = useState(
+  //   document.getElementsByClassName("upper-canvas ")[0]
+  // );
+
+  useEffect(() => {
+    window?.addEventListener("click", () => {
+      setLowerCanvas(
+        document.getElementsByClassName("lower-canvas")[0] as HTMLCanvasElement
+      );
+    });
+  }, []);
+
+  useEffect(() => {
+    const img = lowerCanvas?.toDataURL("image/png");
+    let w = window.open();
+    if (w?.window) w.document.body.innerHTML = "<img src='" + img + "'>";
+
+    console.log("b");
+    if (lowerCanvas?.getContext) {
+      console.log(lowerCanvas.getContext("2d"));
+    }
+  }, [lowerCanvas?.toDataURL("image/png")]);
+
   return (
     <ImageEditor
       includeUI={{
