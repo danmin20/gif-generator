4481ebdd55f26c35e0d249f5412e56352381571e
lee jeongmin <danmin20@khu.ac.kr>
2021-05-27 17:23:26 +0900

    og 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,8 +38,8 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="utf-8" />
-          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-          <meta property="og:title" content="네이버 예약" />
+          {/* <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" /> */}
+          <meta property="og:title" content="git generator" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
